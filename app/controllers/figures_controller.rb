a9bb7fd1982e5735488f80a77e01d6e27012cf9a
Vinny Martinez <vinny@vinnymartinez.com>
2016-12-20 11:52:01 -0500

    so far so bad

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -6,6 +6,13 @@ class FiguresController < ApplicationController
 
   post '/figures' do
     @figure = Figure.create(params[:figure])
+    if params[:landmark][:name].length > 0
+      @figure.landmark << landmark[:name]
+    end
+    if params[:title][:name].length > 0
+      @figure.title << params[:title][:name]
+    end
+    @figure.save
     redirect "/figure/#{@figure.id}"
   end
 
