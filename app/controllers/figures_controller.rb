45cdf9c934bc5bf0eee798d54704424cc73637ce
Jenny Lai <jenny.lai@flatironschool.com>
2016-12-20 11:58:14 -0500

    not done

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -1,16 +1,49 @@
 class FiguresController < ApplicationController
 
+  get '/figures' do
+    erb :'figures/index'
+  end
+
   get '/figures/new' do
     erb :'figures/new'
   end
 
   post '/figures' do
     @figure = Figure.create(params[:figure])
-    redirect "/figure/#{@figure.id}"
+      if !params["landmark"]["name"].empty?
+        @figure.landmarks << Landmark.create(name: params["landmark"]["name"])
+      end
+      @figure.save
+      redirect "/figure/#{@figure.id}"
+    end
+
+  post '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(params["figure"])
+    if !params[""]["name"].empty?
+      @figure.landmarks << Landmark.create(name: params["landmarks"]["name"])
+    end
+    redirect to "/figure/#{@figure.id}"
   end
 
   get '/figures/:id' do
     @figure = Figure.find(params[:id])
     erb :'figures/show'
   end
+
+  get '/figures/:id/edit' do
+    @figure = Figure.find(params[:id])
+    erb :'figures/edit'
+  end
+
+  patch '/figures/:id' do
+    @figure = Figure.find(params[:id])
+    @figure.update(params[:figure])
+    #redirect "/figure/#{@figure.id}" ?
+    erb :'figures/show'
+  end
+
+  #figure - title name
+  #figure - landmark name
+
 end
